1aaf32e4caef0a7c9d2aabb4b6c1af2482a36db3
San-Moro <valentinacortese.dsg@gmail.com>
2022-11-14 17:58:41 +0100

    Added Milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,55 @@
 <script>
+import axios from "axios";
+import SearchBar from "./components/SearchBar.vue";
+import {store} from "./store";
+
 export default {
-  
+  components: {
+    SearchBar
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  created() {
+    axios
+      .get(`https://api.themoviedb.org/3/search/movie?api_key=738c63774d45def498449172fbfdb41f&query=matrix`)
+      .then((resp) => {
+        this.store.movies = resp.data.results;
+      } );
+  },
+  methods: {
+    getMovies() {
+      axios
+        .get(`https://api.themoviedb.org/3/search/movie?api_key=${this.store.apiKey}&query=${this.store.searchKey}`)
+        .then((resp) => {
+        this.store.movies = resp.data.results;
+        console.log(this.store.movies);
+
+      })
+    }
+  }
 }
 </script>
 
 <template>
-  <h1>ciao</h1>
+  <main>
+    <section>
+      <SearchBar @searchClicked="getMovies" />
+    </section>
+    <section class="search-result">
+      <ul>
+        <li v-for="movie in store.movies" :key="movie.id">
+          <h2> Title: {{ movie.title }}</h2>
+          <p> Original Title: {{ movie.original_title }}</p>
+          <p> Original Language: {{ movie.original_language }}</p>
+          <p> Vote: {{ movie.vote_average }}</p>
+
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <style></style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<script>
+import {store} from "../store";
+
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+        store
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+        <label for="search">Cerca cosa guardare: </label>
+        <input type="text" id="search"
+        v-model="store.searchKey">
+        <button class="btn-search" @click="$emit('searchClicked')">Cerca</button>
+    </div>
+</template>
+
+<style></style>
